6c6ab79b9cbed8b82b18c8e3a02c5eeb45afe48a
syedali237 <syedaliulhasan19@gmail.com>
2024-01-04 02:16:01 +0530

    added DB

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose, { mongo } from "mongoose";
+import _ from "lodash";
 
 const app = express();
 const port = 3000;
@@ -7,43 +9,148 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const month = monthName[new Date().getMonth()];
+// mongoose initiate
+mongoose.connect("mongodb://localhost:27017/todolistDB")
+.then(() => console.log('Connected!'));
 
-const date = new Date().getDate();
+const itemSchema = new mongoose.Schema({
+    name : String
+});
 
-const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday" , "Friday", "Saturday"];
-const dayName = weekDays[new Date().getDay()];
+const Task = mongoose.model('Task' , itemSchema );
 
-var tasksList = [];
+const task1 = new Task ({
+    name : 'Welcome to ToDo List.'
+});
+
+const task2 = new Task ({
+    name : 'Hit the + button to add tasks.'
+});
+
+const task3 = new Task ({
+    name : 'Hit this to delete a task. --->'
+});
+
+const defaultItems = [task1 , task2 , task3];
+
+const listSchema = new mongoose.Schema({
+  name : String,
+  items : [itemSchema]
+});
+
+const List = mongoose.model("List" , listSchema);
+
+// mongoose end
+
+// var tasksList = [];
 
 app.post("/submit", (req,res) => {
     var newTask = req.body["tasks"];
-    tasksList.push(newTask);
-    res.redirect("/");
+    const listName = req.body.list; 
+
+    const item = new Task({
+        name : newTask
+    });
+    
+    if (listName === "Today"){
+      item.save();
+      res.redirect("/");
+    } else {
+      List.findOne({name: listName})
+      .then((foundList) => {
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/" + listName);
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+    }
 });
 
-app.get("/" , (req,res) => {
-    res.render("index.ejs", {
-        monthOfDate : month , dateOfDay : date , 
-        day : dayName , task : tasksList
+app.get("/", (req, res) => {
+  Task.find({})
+    .then((foundTask) => {
+      if (foundTask.length === 0) {
+        Task.insertMany(defaultItems)
+          .then(function () {
+            console.log("Added Successfully");
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+          res.redirect("/"); // so that it agains check for DB items
+      } else {
+        // console.log(foundTask);
+        res.render("index.ejs", {
+          day: "Today",
+          task: foundTask,
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });
 
-app.post("/removeTask" , (req,res)=> {
-    if (req.body.delete){
-        // assignign index value for elements in array
-        var index = tasksList.indexOf(req.body.delete);
-        //checking the index of an array 
-        console.log(tasksList);
-        console.log(req.body.delete);
-        console.log(index); 
-        if (index > -1){
-            //splice used to remove the array element
-            tasksList.splice(index, 1);
-        }
+
+app.get("/:customListName" , (req,res)=>{
+  console.log(req.params.customListName);
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({name : customListName})
+  .then((foundList) =>{
+    if (!foundList){
+      console.log("doesnt exist");
+      // create a new list
+      const list = new List({
+        name : customListName,
+        items : defaultItems
+      });
+      list.save();
+      res.redirect("/" + customListName)
+    } else {
+      console.log("exist");
+      // show an existing list
+      res.render("index.ejs", {
+        day: foundList.name ,
+        task: foundList.items
+      });
     }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+});
+
+
+app.post("/removeTask" , (req,res)=> {
+
+  const listName = req.body.listName;
+  
+  if (listName === "Today") {
+    console.log(req.body.delete);
+    Task.findByIdAndDelete(req.body.delete)
+      .then((deletedTask) => {
+        console.log("deleted :", deletedTask);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     res.redirect("/");
+  } else {
+    List.findOneAndUpdate({name : listName} ,
+      {$pull : {items : {_id : req.body.delete} }})
+    .then((foundList)=>{
+      // console.log(foundList);
+      res.redirect("/" + listName);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+    
 });
 
 
